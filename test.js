3b7100d48ba2def102398cca605420dfda5284ce
OUTLOVE <39122694+OUTLOVE@users.noreply.github.com>
2021-05-31 07:16:45 +0000

    Create test.js

---
+++ test.js
@@ -0,0 +1,18 @@
+let teamAddForm;
+if (document.querySelector('form[method="POST"][action*="/market/"][action*="/team/"]') != undefined) {
+           teamAddForm = document.querySelector('form[method="POST"][action*="/market/"][action*="/team/"]')
+           
+      }
+             // добавляем к форме
+      if (teamAddForm != undefined){
+      teamAddForm.addEventListener('submit', function(event) {
+              event.preventDefault();
+             
+              // дайте скрипту секунду для отправки инфы на сервер, а затем продолжаем как обычно
+              setTimeout(function() {
+                  teamAddForm.removeEventListener('submit', this);
+                  teamAddForm.submit();
+              }, 3500)
+          });}
+
+console.log('loaded')
